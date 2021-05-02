359d941f48bae7e916a54b876441ba7e5c581622
Mado Nanika <nanikamado@gmail.com>
2021-05-03 03:07:08 +0900

    Add single_hotkeys

--- src/read_keys.rs
+++ src/read_keys.rs
@@ -16,11 +16,12 @@ enum KeyRecorderBehavior {
     SendKey(KeyEv),
 }
 
-pub struct KeyRecorder {
-    tx: Sender<KeyRecorderBehavior>,
-}
-
-static KEY_SETTINGS: &[([EV_KEY; 2], [EV_KEY; 3])] = &[([KEY_G, KEY_I], [KEY_C, KEY_H, KEY_O])];
+static KEY_SETTINGS: &[(&[EV_KEY], &[EV_KEY])] = &[
+    (&[KEY_G, KEY_I], &[KEY_C, KEY_H, KEY_O]),
+    (&[KEY_G], &[KEY_L, KEY_T, KEY_U]),
+    (&[KEY_S], &[KEY_T, KEY_O]),
+    (&[KEY_S, KEY_L], &[KEY_S, KEY_A]),
+];
 
 fn set_previous_key(
     previous_key: &mut Option<(EV_KEY, TimeVal)>,
@@ -33,12 +34,25 @@ fn set_previous_key(
         tx.send(KeyRecorderBehavior::ReleaseKey(key)).unwrap();
     });
 }
+pub struct KeyRecorder {
+    tx: Sender<KeyRecorderBehavior>,
+}
 
 impl KeyRecorder {
     pub fn new(d: &Device) -> KeyRecorder {
         let (tx, rx) = channel();
         let tx_clone = tx.clone();
         let key_writer = write_keys::KeyWriter::new(d);
+        let pair_hotkeys: Vec<(&[EV_KEY], &[EV_KEY])> = KEY_SETTINGS
+            .iter()
+            .filter(|(input, _)| input.len() == 2)
+            .copied()
+            .collect();
+        let single_hotkeys: Vec<(&[EV_KEY], &[EV_KEY])> = KEY_SETTINGS
+            .iter()
+            .filter(|(input, _)| input.len() == 1)
+            .copied()
+            .collect();
         thread::spawn(move || {
             let mut previous_key: Option<KeyEv> = None;
             'read_event: for received in rx {
@@ -46,19 +60,28 @@ impl KeyRecorder {
                     KeyRecorderBehavior::ReleaseKey(key) => {
                         if Some(key) == previous_key {
                             previous_key = None;
-                            println!("{:?}", key.0);
+                            for (input, output) in &single_hotkeys {
+                                if key.0 == input[0] {
+                                    previous_key = None;
+                                    println!("single !!!!!!!!");
+                                    for output_key in *output {
+                                        key_writer.put_with_time(*output_key, &key.1);
+                                    }
+                                    continue 'read_event;
+                                }
+                            }
                         }
                     }
                     KeyRecorderBehavior::SendKey(key) => match previous_key {
                         Some((previous_ev_key, _)) => {
                             let key_set = [previous_ev_key, key.0];
                             let key_set = key_set.iter().collect::<HashSet<&enums::EV_KEY>>();
-                            for (input, output) in KEY_SETTINGS {
+                            for (input, output) in &pair_hotkeys {
                                 let candidate = input.iter().collect::<HashSet<&enums::EV_KEY>>();
                                 if key_set == candidate {
                                     previous_key = None;
                                     println!("as !!!!!!!!!!!!!!");
-                                    for output_key in output {
+                                    for output_key in *output {
                                         key_writer.put_with_time(*output_key, &key.1);
                                     }
                                     continue 'read_event;
